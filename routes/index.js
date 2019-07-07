0394d0d28606a8a0e1451043b3905758af18525c
Prajjawal-Prayank <prajjawal.prayank@gmail.com>
2019-07-07 13:42:41 +0530

    updated opening page

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var fs=require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { code:0 });           
+  res.render('admin_login',{code:0});          
 });
 
 
